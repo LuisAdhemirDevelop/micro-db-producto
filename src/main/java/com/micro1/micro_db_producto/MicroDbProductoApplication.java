7a58833bcd99229d5c525d9ae6b4e47e242d703a
Luis Añazgo <luis.adhemir.develop@gmail.com>
2025-11-27 17:44:14 -0500

    subiendo repositorio

---
+++ src/main/java/com/micro1/micro_db_producto/MicroDbProductoApplication.java
@@ -0,0 +1,13 @@
+package com.micro1.micro_db_producto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroDbProductoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MicroDbProductoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/micro1/micro_db_producto/MicroDbProductoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.micro1.micro_db_producto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroDbProductoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
